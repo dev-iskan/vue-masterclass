ce83e4731b32029a39ffb5fbb7150362177b5d80
dev-iskan <successfuliskan@gmail.com>
2018-11-30 11:35:41 +0500

    fixing styles and data preview

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 1
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<template lang="pug">
+  div
+    div(v-for="thread in threads" :key="thread['.key']" class="col-large push-top")
+      h1 {{thread.title}}
+      div(class="post-list")
+        div(v-for="postId in thread.posts" :key="postId['.key']" class="post")
+          div(class="user-info")
+            a(href="#" class="user-name") {{users[posts[postId].userId].name}}
+            a(href="#")
+              img(class="avatar-large" :src="users[posts[postId].userId].avatar" alt="")
+            p(class="desktop-only text-small") 107 posts
+          div(class="post-content")
+            div {{posts[postId].text}}
+          div(class="post-date text-faded") {{posts[postId].publishedAt}}
+  </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import sourceData from '@/data.json'
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
   }
 }
 </script>
+
+<style>
+@import "../assets/css/style.css";
+</style>
